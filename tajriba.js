3dc729f640e14eae817fcfe3c32312b39e756812
JamShidBek2003 <92529675+Jamsh1dbe@users.noreply.github.com>
2021-11-19 15:18:31 +0500

    test

---
+++ tajriba.js
@@ -0,0 +1,32 @@
+
+
+// console.log(parseFloat('12.3sahsabhjdb'));
+// console.log(parseFloat(('12,3').toFixed(2)));
+// skd ='213'
+// console.log((eval (skd)).repeat(3));
+// kjdsk = '         dasfnksfnakf    rwe '
+// console.log(kjdsk.trim());
+// console.log(kjdsk);
+
+
+// var msn ='covid2019'
+// if()
+
+// console.log(eval('213+21'));
+// var kdsa='jamshid'
+// console.log(kdsa.search('a'));
+// console.log(kdsa.search('j'));
+// console.log(kdsa.search('j'));
+// var a=(console.log(kdsa.search('a'),kdsa.search('j'),kdsa.search('j')))
+// console.log(a.join());
+
+// console.log(namee);
+// var namee;
+
+
+// var ar = 'AAAADDFFFGGTAAOOOLKKKKMH';
+// for(i=0; i<ar.length; i++){
+
+// }
+
+
